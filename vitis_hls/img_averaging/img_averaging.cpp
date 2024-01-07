1f19e073e2436cfb8dca26586a8cd15be63f47ce
Marco Aiello <caccolillo@yahoo.com>
2024-01-07 10:57:10 +0000

    Reworked the code so to be used as xilinx runtime kernel HW accelerator.

--- vitis_hls/img_averaging/img_averaging.cpp
+++ vitis_hls/img_averaging/img_averaging.cpp
@@ -1,22 +1,78 @@
+// Includes
+#include <stdint.h>
+#include <hls_stream.h>
 
+#define DATA_SIZE 4096
+
+// TRIPCOUNT identifier
+const int c_size = DATA_SIZE;
+
+static void load_input(uint32_t* in, hls::stream<uint32_t>& inStream, int size) {
+mem_rd:
+    for (int i = 0; i < size; i++) {
+		#pragma HLS LOOP_TRIPCOUNT max=4096 min=4096
+        inStream << in[i];
+    }
+}
+
+static void img_averaging(hls::stream<uint32_t>& in1_stream,
+                        hls::stream<uint32_t>& in2_stream,
+                        hls::stream<uint32_t>& out_stream,
+						int scale_by_4,
+                        int size) {
+    int sum;
+    int out_val;
+
+    // The kernel is operating with vector of NUM_WORDS integers. The + operator performs
+    // an element-wise add, resulting in NUM_WORDS parallel additions.
+    execute:
+    for (int i = 0; i < size; i++) {
+		#pragma HLS LOOP_TRIPCOUNT max=4096 min=4096
+    	sum = (in1_stream.read() + in2_stream.read());
+        out_val = scale_by_4 ? sum/4 : sum;
+    	out_stream << out_val;
+    }
+}
+
+
+static void store_result(uint32_t* out, hls::stream<uint32_t>& out_stream, int size) {
+mem_wr:
+    for (int i = 0; i < size; i++) {
+		#pragma HLS LOOP_TRIPCOUNT max=4096 min=4096
+    	out[i] = out_stream.read();
+    }
+}
 
 extern "C" {
-	void img_averaging(
-	        const unsigned int *in1, // Read-Only Vector 1
-	        const unsigned int *in2, // Read-Only Vector 2
-	        unsigned int *out,       // Output Result
-	        int scale_by_4,          //when 1, scale output by 4
-	        int size                 // Size in integer
-	        )
-	{
-#pragma HLS INTERFACE m_axi port=in1 bundle=aximm1
-#pragma HLS INTERFACE m_axi port=in2 bundle=aximm2
-#pragma HLS INTERFACE m_axi port=out bundle=aximm1
-           int sum;
-	    for(int i = 0; i < size; ++i)
-	    {
-	        sum = in1[i] + in2[i];
-	        scale_by_4 ? sum/4 : sum;
-	    }
+
+	/*
+		Vector Addition Kernel
+
+		Arguments:
+			in1  (input)  		--> Input vector 1
+			in2  (input)  		--> Input vector 2
+			out  (output) 		--> Output vector
+			scale_by_4 (input)  --> When 1, scale output by 4
+			size (input)  		--> Number of elements in vector
+	*/
+
+	void krnl_img_averaging(uint32_t* in1, uint32_t* in2, uint32_t* out, int scale_by_4, int size) {
+		#pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
+		#pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
+		#pragma HLS INTERFACE m_axi port = out bundle = gmem0
+
+		static hls::stream<uint32_t> in1_stream("input_stream_1");
+		static hls::stream<uint32_t> in2_stream("input_stream_2");
+		static hls::stream<uint32_t> out_stream("output_stream");
+
+		#pragma HLS dataflow
+		// dataflow pragma instruct compiler to run following three APIs in parallel
+		load_input(in1, in1_stream, size);
+		load_input(in2, in2_stream, size);
+		img_averaging(in1_stream, in2_stream, out_stream, scale_by_4, size);
+		store_result(out, out_stream, size);
 	}
 }
+
+
+
--- vitis_hls/img_averaging/script.tcl
+++ vitis_hls/img_averaging/script.tcl
@@ -4,13 +4,17 @@
 ## Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ############################################################
 open_project img_averaging_prj
-set_top img_averaging
+set_top krnl_img_averaging
 add_files img_averaging.cpp
+add_files img_averaging.h
+add_files -tb img_averaging_tb.cpp
 open_solution "solution1" -flow_target vitis
 set_part {xczu3eg-sbva484-1-e}
 create_clock -period 10 -name default
-#source "./img_averaging_prj/solution1/directives.tcl"
-#csim_design
+config_interface -m_axi_alignment_byte_size 64 -m_axi_latency 64 -m_axi_max_widen_bitwidth 512 -m_axi_offset slave
+config_rtl -register_reset_num 3
+source "./directives.tcl"
+csim_design
 csynth_design
-#cosim_design
-export_design -format ip_catalog
+cosim_design
+export_design -rtl verilog -format xo
