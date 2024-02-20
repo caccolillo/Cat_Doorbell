ce80a82fb3e278d9a8ca988aac4ca4227be23fd5
Marco Aiello <caccolillo@yahoo.com>
2024-02-20 20:52:43 +0000

    Added lines to get an SDK to compile SW on Linux.

--- petalinux/build_linux_distro.sh
+++ petalinux/build_linux_distro.sh
@@ -6,6 +6,8 @@ petalinux-config --get-hw-description ../bd_wrapper.xsa
 petalinux-config
 petalinux-config -c rootfs
 petalinux-config -c kernel
-petalinux-build -c petalinux-image-full
+petalinux-build 
+petalinux-build --sdk
+petalinux-package --sysroot
 cd /images/linux
 petalinux-package --boot --fsbl zynqmp_fsbl.elf --u-boot u-boot.elf --pmufw pmufw.elf --fpga system.bit
