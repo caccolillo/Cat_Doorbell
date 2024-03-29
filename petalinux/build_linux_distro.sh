251f91b9f1a7cc160bffec3f3b58eb1ecd2002ed
Marco Aiello <caccolillo@yahoo.com>
2024-03-29 09:54:29 +0000

    Modified the script so to use local copies of the downloads and sstate-cache folders, to add the img_avg_kernel HLS device to the device tree source file.

--- petalinux/build_linux_distro.sh
+++ petalinux/build_linux_distro.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-source /home/caccolillo/Documents/Xilinx/petalinux/settings.sh
-petalinux-create -t project --template zynqMP --name petalinux_prj
-cd petalinux_prj
+petalinux-create -t project -s ./u96v2_sbc_base_2020_2.bsp
+cd u96v2_sbc_base_2020_2
 petalinux-config --get-hw-description ../bd_wrapper.xsa
-petalinux-config
-petalinux-config -c rootfs
-petalinux-config -c kernel
-petalinux-build 
+#petalinux-config
+#petalinux-config -c rootfs
+#petalinux-config -c kernel
+#petalinux-build 
+cp ../petalinuxbsp.conf ./project-spec/meta-user/conf
+cp ../system-user.dtsi ./project-spec/meta-user/recipes-bsp/device-tree/files
+petalinux-build -c avnet-image-full
 petalinux-build --sdk
 petalinux-package --sysroot
 cd /images/linux
 petalinux-package --boot --fsbl zynqmp_fsbl.elf --u-boot u-boot.elf --pmufw pmufw.elf --fpga system.bit
+petalinux-boot --qemu --prebuilt 3
